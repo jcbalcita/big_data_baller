cb2144da4bc5e1753fbb4780adc96bf0b957f446
John Carlo Aspiras Balcita <john.balcita@gmail.com>
2018-12-11 21:19:33 -0800

    writes multiple days to s3

--- lib/big_data_baller.ex
+++ lib/big_data_baller.ex
@@ -4,27 +4,39 @@ defmodule BigDataBaller do
   @game_date_format "{M}/{D}/{YYYY}"
   @s3_bucket_name "nba-box-scores-s3"
 
-  def run do
-    Nba.Stats.scoreboard(%{"gameDate" => date_string(yesterday(), @game_date_format)})["GameHeader"]
-    |> Enum.each(fn header ->
-      gid = header["GAME_ID"]
-      [_, game_code] = String.split(header["GAMECODE"], "/")
-      year_month_day = Timex.format!(yesterday, "{YYYY}/{0M}/{0D}")
-      season = header["SEASON"]
-      s3_path = "#{season}/#{year_month_day}/#{gid}-#{game_code}"
-
-      Nba.Stats.box_score(%{"GameID" => gid})
-      |> Poison.encode!()
-      |> write_to_s3(s3_path)
+  def run({year, month, start_day}, end_day) do
+    (start_day..end_day)
+    |> Enum.each(fn day ->
+      fetch(Timex.to_datetime({year, month, day}))
     end)
   end
 
-  def yesterday do
-    Timex.subtract(Timex.now(Timex.Timezone.local), Timex.Duration.from_days(1))
+  def fetch(date_time) do
+    case Nba.Stats.scoreboard(%{"gameDate" => date_string(date_time, @game_date_format)})["GameHeader"] do
+      nil -> 
+        IO.puts("you got rate limited, dumbass")
+
+      headers ->
+        headers
+        |> Enum.map(&Task.async(fn -> handle_header(&1, date_time) end))
+        |> Enum.map(&Task.await/1)
+    end
   end
 
   def date_string(date_time, format) do
     Timex.format!(date_time, format)
+  end
+
+  def handle_header(header, date_time) do
+    gid = header["GAME_ID"]
+    [_, game_code] = String.split(header["GAMECODE"], "/")
+    year_month_day = Timex.format!(date_time, "{YYYY}/{0M}/{0D}")
+    season = header["SEASON"]
+    s3_path = "#{season}/#{year_month_day}/#{gid}-#{game_code}.json"
+
+    Nba.Stats.box_score(%{"GameID" => gid})
+    |> Poison.encode!()
+    |> write_to_s3(s3_path)
   end
 
   def write_to_s3(json, path) do
