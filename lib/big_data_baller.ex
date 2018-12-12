2606b89ba79b0a4a3cc1c9f301352378e79b6982
John Carlo Aspiras Balcita <john.balcita@gmail.com>
2018-12-11 21:49:51 -0800

    pass correct season param

--- lib/big_data_baller.ex
+++ lib/big_data_baller.ex
@@ -5,7 +5,7 @@ defmodule BigDataBaller do
   @s3_bucket_name "nba-box-scores-s3"
 
   def run({year, month, start_day}, end_day) do
-    (start_day..end_day)
+    start_day..end_day
     |> Enum.each(fn day ->
       fetch(Timex.to_datetime({year, month, day}))
     end)
@@ -13,7 +13,7 @@ defmodule BigDataBaller do
 
   def fetch(date_time) do
     case Nba.Stats.scoreboard(%{"gameDate" => date_string(date_time, @game_date_format)})["GameHeader"] do
-      nil -> 
+      nil ->
         IO.puts("you got rate limited, dumbass")
 
       headers ->
@@ -28,13 +28,17 @@ defmodule BigDataBaller do
   end
 
   def handle_header(header, date_time) do
+    season =
+      Timex.format!(date_time, "{YYYY}") <>
+        "-" <> Timex.format!(Timex.add(date_time, Timex.Duration.from_days(367)), "{YY}")
+
+    season_dir = header["SEASON"]
     gid = header["GAME_ID"]
     [_, game_code] = String.split(header["GAMECODE"], "/")
     year_month_day = Timex.format!(date_time, "{YYYY}/{0M}/{0D}")
-    season = header["SEASON"]
-    s3_path = "#{season}/#{year_month_day}/#{gid}-#{game_code}.json"
+    s3_path = "#{season_dir}/#{year_month_day}/#{gid}-#{game_code}.json"
 
-    Nba.Stats.box_score(%{"GameID" => gid})
+    Nba.Stats.box_score(%{"GameID" => gid, "Season" => season})
     |> Poison.encode!()
     |> write_to_s3(s3_path)
   end
