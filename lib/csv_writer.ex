6daa5dc9ff26f9f18050df13bd02aee13ebe9bf3
jbalcita <jbalcita@cj.com>
2019-01-04 15:59:29 -0800

    write json to csv

--- lib/csv_writer.ex
+++ lib/csv_writer.ex
@@ -1,7 +1,59 @@
 defmodule BigDataBaller.CsvWriter do
-  
-  def json_to_csv do 
-    Path.wildcard("syncS3/2*/**/*.json") 
-    |> Enum.each(&IO.puts(&1))
+
+  def json_to_csv(year) do
+    File.touch("#{year}.csv")
+    {:ok, file} = File.open("#{year}.csv", [:write, :utf8])
+
+    Path.wildcard("syncS3/#{year}/**/*.json")
+    |> convert_to_matrix()
+    |> CSV.encode()
+    |> Enum.each(&IO.write(file, &1))
+
+    File.close(file)
+  end
+
+  defp convert_to_matrix(filepaths) do
+    filepaths
+    |> Enum.reduce([], fn file, acc ->
+      IO.inspect file
+      Enum.concat(acc, player_rows(file))
+    end)
+  end
+
+  defp player_rows(file) do
+    json_map =
+      with {:ok, body} <- File.read(file),
+           {:ok, json} <- Poison.decode(body),
+      do: json
+
+    Map.get(json_map, "PlayerStats")
+    |> Enum.map(&player_map_to_list/1)
+  end
+
+  defp player_map_to_list(player_map) do
+    [
+      player_map["PLAYER_ID"],
+      player_map["MIN"],
+      player_map["PTS"],
+      player_map["OREB"],
+      player_map["DREB"],
+      player_map["REB"],
+      player_map["AST"],
+      player_map["STL"],
+      player_map["BLK"],
+      player_map["TO"],
+      player_map["FTM"],
+      player_map["FTA"],
+      player_map["FGM"],
+      player_map["FGA"],
+      player_map["FG3M"],
+      player_map["FG3A"],
+      player_map["PLAYER_NAME"],
+      player_map["TEAM_ID"],
+      player_map["TEAM_ABBREVIATION"],
+      player_map["START_POSITION"],
+      player_map["PLUS_MINUS"],
+      player_map["GAME_ID"]
+    ]
   end
 end
