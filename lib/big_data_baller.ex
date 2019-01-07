23bfa51b256b33f8ce9e869cfa0fa6a0739e06f4
John Carlo Aspiras Balcita <john.balcita@gmail.com>
2019-01-07 08:12:19 -0800

    can fetch box scores for a date range

--- lib/big_data_baller.ex
+++ lib/big_data_baller.ex
@@ -5,27 +5,29 @@ defmodule BigDataBaller do
   @s3_directory_format "{YYYY}/{0M}/{0D}"
   @s3_bucket_name "nba-box-scores-s3"
 
-  def box_scores({year, month, start_day}, end_day) do
-    start_day..end_day
-    |> Enum.each(fn day ->
-      fetch_box_score(Timex.to_datetime({year, month, day}))
-    end)
+  def box_scores(start_date, end_date) do
+    with {:ok, start_datetime, end_datetime} <- get_datetimes(start_date, end_date) do
+      step_through_days(start_datetime, end_datetime)
+      IO.puts("Completed fetching box scores for the specified time range")
+    else
+      _ -> IO.puts("Bad dates...")
+    end
   end
 
-  def fetch_box_score(date_time) do
-    case Nba.Stats.scoreboard(%{"gameDate" => Timex.format!(date_time, @game_date_format)})["GameHeader"] do
+  def fetch_box_scores(datetime) do
+    case Nba.Stats.scoreboard(%{"gameDate" => Timex.format!(datetime, @game_date_format)})["GameHeader"] do
       nil ->
         IO.puts("you got rate limited, dumbass")
 
       game_headers ->
         game_headers
-        # |> Enum.map(&Task.async(fn -> process_game(&1, date_time) end))
-        |> Enum.each(fn header -> process_game(header, date_time) end)
+        # |> Enum.map(&Task.async(fn -> process_game(&1, datetime) end))
+        |> Enum.each(fn header -> process_game(header, datetime) end)
         # |> Enum.map(&Task.await/1)
     end
   end
 
-  def process_game(header, date_time) do
+  defp process_game(header, datetime) do
     season_start_year = header["SEASON"]
     season_end_year =
       elem(Integer.parse(season_start_year), 0) + 1
@@ -34,8 +36,10 @@ defmodule BigDataBaller do
 
     gid = header["GAME_ID"]
     [_, game_code] = String.split(header["GAMECODE"], "/")
-    year_month_day = Timex.format!(date_time, @s3_directory_format)
+    year_month_day = Timex.format!(datetime, @s3_directory_format)
     s3_path = "#{season_start_year}/#{year_month_day}/#{gid}-#{game_code}.json"
+
+    Process.sleep(1000)
 
     Nba.Stats.box_score(%{"GameID" => gid, "Season" => "#{season_start_year}-#{season_end_year}"})
     |> Poison.encode!()
@@ -45,5 +49,22 @@ defmodule BigDataBaller do
   def write_to_s3(json, path) do
     ExAws.S3.put_object(@s3_bucket_name, path, json)
     |> ExAws.request()
+  end
+
+  defp get_datetimes(start_date, end_date) do
+    case {Timex.to_datetime(start_date), Timex.to_datetime(end_date)} do
+      {{:error, _}, _} -> :error
+      {_, {:error, _}} -> :error
+      {sdt, edt} -> {:ok, sdt, edt}
+    end
+  end
+
+  defp step_through_days(datetime, end_datetime) do
+    case Timex.after?(datetime, end_datetime) do 
+      true -> IO.puts("Done stepping through days")
+      false -> 
+        fetch_box_scores(datetime)
+        step_through_days(Timex.add(datetime, Timex.Duration.from_days(1)), end_datetime)
+    end
   end
 end
