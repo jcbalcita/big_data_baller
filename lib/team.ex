ae17afdb2008d0717178f94b559150e29678f117
jbalcita <jbalcita@cj.com>
2019-01-24 12:59:28 -0800

    move stuff around, create modules
    - util module for junk-drawer kind of stuff
    - skeleton for team stats module

--- lib/team.ex
+++ lib/team.ex
@@ -1,34 +1,8 @@
 defmodule BigDataBaller.Team do
-  alias BigDataBaller.Util
-
-  @measure_types %{
-    four_factors: "Four Factors",
-    advanced: "Advanced",
-    scoring: "Scoring",
-    opponent: "Opponent",
-    defense: "Defense"
-  }
-
-  def general(start_year \\ 1996, end_year \\ 2018) do
-    Enum.each(start_year..end_year, fn year -> 
-      Enum.each(Map.keys(@measure_types), fn measure_type -> 
-        general_stats_by_year({measure_type, year})
-      end)
-    end)
+  def four_factors(start_year \\ 1996, end_year \\ 2019) do
+    Enum.each(start_year..end_year, &four_factors_by_year/1)
   end
 
-  def general_stats_by_year({atom, year}) do
-    with season_suffix <- Util.season_year_suffix(year),
-         measure_type <- @measure_types[atom],
-         s3_path <- "team/#{atom}/#{year}.json",
-         {:ok, result} <- Nba.Stats.team_stats(%{"MeasureType" => measure_type, "Season" => "#{year}-#{season_suffix}"}) do
-      
-      Process.sleep(999)
-
-      Poison.encode!(result)
-      |> Util.write_to_s3(s3_path)
-    else
-      {:error, message} -> IO.puts message
-    end
+  defp four_factors_by_year(year) do
   end
 end
